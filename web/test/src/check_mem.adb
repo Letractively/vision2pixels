d82763bca8101da33f3e0610c4b04c7a888a6a26
olivier.ramonat <olivier.ramonat@33d37de6-082f-0410-8f52-fde41cb23233>
2007-07-05 19:32:40 +0000

    Adds check_mem project

---
+++ web/test/src/check_mem.adb
@@ -0,0 +1,62 @@
+------------------------------------------------------------------------------
+--                              Vision2Pixels                               --
+--                                                                          --
+--                           Copyright (C) 2006                             --
+--                      Pascal Obry - Olivier Ramonat                       --
+--                                                                          --
+--  This library is free software; you can redistribute it and/or modify    --
+--  it under the terms of the GNU General Public License as published by    --
+--  the Free Software Foundation; either version 2 of the License, or (at   --
+--  your option) any later version.                                         --
+--                                                                          --
+--  This library is distributed in the hope that it will be useful, but     --
+--  WITHOUT ANY WARRANTY; without even the implied warranty of              --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       --
+--  General Public License for more details.                                --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License       --
+--  along with this library; if not, write to the Free Software Foundation, --
+--  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.       --
+------------------------------------------------------------------------------
+
+with Ada.Command_Line;
+with AUnit;
+
+with Gwiad.Web;
+with Gwiad.Dynamic_Libraries.Manager;
+with V2P.Web_Server;
+
+with Web_Suite;
+
+procedure Check_Mem is
+   use Ada;
+
+   use Gwiad;
+
+   procedure Run is new AUnit.Test_Runner (Web_Suite);
+
+begin
+
+--   Web.Start;
+
+   declare
+      use Ada.Command_Line;
+      use Gwiad.Dynamic_Libraries.Manager;
+      Iteration : constant Positive := Positive'Value (Argument (1));
+   begin
+
+      for K in 1 .. Iteration loop
+         --  Run tests
+         Manager.Discover_Libraries;
+         Run;
+         delay 1.0;
+         Manager.Unload_All;
+         delay 1.0;
+      end loop;
+   end;
+
+   --  Exit now
+
+   Web.Stop;
+
+end Check_Mem;
